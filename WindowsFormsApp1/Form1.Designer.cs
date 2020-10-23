964d3d89574a124f4569da4a49ef20a24dea4ae6
vladpepe <53265456+vladpepe@users.noreply.github.com>
2020-10-23 10:46:02 +0200

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.Panel();
+            this.pictureBox5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.vlad = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.over = new System.Windows.Forms.Label();
+            this.lives = new System.Windows.Forms.Label();
+            this.coins = new System.Windows.Forms.Label();
+            this.floor = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.vlad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(179, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 63);
+            this.pictureBox1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(179, 108);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 63);
+            this.pictureBox2.TabIndex = 1;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(179, 199);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 63);
+            this.pictureBox3.TabIndex = 2;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.White;
+            this.pictureBox4.Location = new System.Drawing.Point(179, 300);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 63);
+            this.pictureBox4.TabIndex = 3;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.White;
+            this.pictureBox5.Location = new System.Drawing.Point(179, 400);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 63);
+            this.pictureBox5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.White;
+            this.panel6.Location = new System.Drawing.Point(0, -1);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(10, 464);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.White;
+            this.panel7.Location = new System.Drawing.Point(374, -1);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(10, 464);
+            this.panel7.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // vlad
+            // 
+            this.vlad.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vlad.Image = ((System.Drawing.Image)(resources.GetObject("vlad.Image")));
+            this.vlad.Location = new System.Drawing.Point(40, 385);
+            this.vlad.Name = "vlad";
+            this.vlad.Size = new System.Drawing.Size(34, 64);
+            this.vlad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.vlad.TabIndex = 7;
+            this.vlad.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(38, 82);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(34, 42);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 11;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(139, 22);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(34, 42);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 12;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(300, 91);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(34, 42);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 13;
+            this.pictureBox8.TabStop = false;
+            // 
+            // over
+            // 
+            this.over.AutoSize = true;
+            this.over.BackColor = System.Drawing.Color.Red;
+            this.over.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.over.Location = new System.Drawing.Point(92, 157);
+            this.over.Name = "over";
+            this.over.Size = new System.Drawing.Size(191, 39);
+            this.over.TabIndex = 14;
+            this.over.Text = "Game Over";
+            // 
+            // lives
+            // 
+            this.lives.AutoSize = true;
+            this.lives.BackColor = System.Drawing.Color.Transparent;
+            this.lives.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lives.ForeColor = System.Drawing.Color.Red;
+            this.lives.Location = new System.Drawing.Point(296, 5);
+            this.lives.Name = "lives";
+            this.lives.Size = new System.Drawing.Size(50, 20);
+            this.lives.TabIndex = 15;
+            this.lives.Text = "Lives";
+            // 
+            // coins
+            // 
+            this.coins.AutoSize = true;
+            this.coins.BackColor = System.Drawing.Color.Transparent;
+            this.coins.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coins.ForeColor = System.Drawing.Color.Red;
+            this.coins.Location = new System.Drawing.Point(296, 29);
+            this.coins.Name = "coins";
+            this.coins.Size = new System.Drawing.Size(56, 20);
+            this.coins.TabIndex = 16;
+            this.coins.Text = "Score";
+            // 
+            // floor
+            // 
+            this.floor.AccessibleName = "";
+            this.floor.Location = new System.Drawing.Point(12, 450);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(359, 10);
+            this.floor.TabIndex = 17;
+            this.floor.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.floor);
+            this.Controls.Add(this.coins);
+            this.Controls.Add(this.lives);
+            this.Controls.Add(this.over);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vlad);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vlad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pictureBox1;
+        private System.Windows.Forms.Panel pictureBox2;
+        private System.Windows.Forms.Panel pictureBox3;
+        private System.Windows.Forms.Panel pictureBox4;
+        private System.Windows.Forms.Panel pictureBox5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Timer timer1;
+        public PictureBox vlad;
+        private Timer timer2;
+        private Label label1;
+        private Label label2;
+        public PictureBox pictureBox6;
+        public PictureBox pictureBox7;
+        public PictureBox pictureBox8;
+        private Label over;
+        private Label lives;
+        private Label coins;
+        private PictureBox floor;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            over.Visible = false;
+            floor.Visible = false;
+            
+        }
+
+        
+
+        int collectedCoins = 0;
+        int gamespeed = 4;
+        int hearts = 3;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveLine(gamespeed);
+            enemy(gamespeed);
+            gameOver();
+        }
+
+        void moveLine(int speed)
+        {
+            if (pictureBox1.Top >= 500)
+            {  pictureBox1.Top = 0; }
+            else
+            { pictureBox1.Top += speed; }
+
+            if (pictureBox2.Top >= 500)
+            { pictureBox2.Top = 0; }
+            else
+            {pictureBox2.Top += speed;}
+
+            if (pictureBox3.Top >= 500)
+            { pictureBox3.Top = 0; }
+            else
+            { pictureBox3.Top += speed; }
+
+            if (pictureBox4.Top >= 500)
+            { pictureBox4.Top = 0; }
+            else
+            { pictureBox4.Top += speed; }
+
+            if (pictureBox5.Top >= 500)
+            { pictureBox5.Top = 0; }
+            else
+            { pictureBox5.Top += speed; }
+        }
+
+        Random r = new Random();
+        int x, y;
+
+        void enemy(int speed) {
+
+            if (pictureBox6.Top >= 500)
+            {
+                x = r.Next(0, 200);
+                pictureBox6.Location = new Point(x, 0);
+             }
+            else
+            { pictureBox6.Top += speed; }
+
+
+            if (pictureBox7.Top >= 500)
+            {
+                x = r.Next(0, 350);
+                pictureBox7.Location = new Point(x, 0);
+            }
+            else
+            { pictureBox7.Top += speed; }
+
+            if (pictureBox8.Top >= 500)
+            {
+                x = r.Next(200, 350);
+                pictureBox8.Location = new Point(x, 0);
+            }
+            else
+            { pictureBox8.Top += speed; }
+
+        }
+
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (MousePosition.X > 10 && MousePosition.X < 340)
+            {
+                var screen = Screen.FromPoint(Location);
+                vlad.Left = MousePosition.X;
+                label1.Text = vlad.Left.ToString();
+                label2.Text = MousePosition.X.ToString();
+                coinsCollection();
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        void coinsCollection()
+        {
+            if (vlad.Bounds.IntersectsWith(pictureBox6.Bounds))
+            {
+                collectedCoins++;
+                coins.Text = "Coins:" + collectedCoins.ToString();
+                x = r.Next(50, 300);
+                pictureBox6.Location = new Point(x, 0);
+                gamespeed += 1;
+            }
+
+            if (vlad.Bounds.IntersectsWith(pictureBox7.Bounds))
+            {
+                collectedCoins++;
+                coins.Text = "Alina:" + collectedCoins.ToString();
+                x = r.Next(50, 300);
+                pictureBox7.Location = new Point(x, 0);
+                gamespeed += 0;
+            }
+
+            if (vlad.Bounds.IntersectsWith(pictureBox8.Bounds))
+            {
+                collectedCoins++;
+                coins.Text = "Alina:" + collectedCoins.ToString();
+                x = r.Next(50, 300);
+                pictureBox8.Location = new Point(x, 0);
+                gamespeed += 1;
+            }
+            coins.Text = "Alina:" + collectedCoins.ToString();
+
+        }
+
+
+        void gameOver()
+        {
+
+            if (hearts < 1)
+            {
+                over.Visible = true;
+                timer1.Enabled = false;
+            }
+            if (floor.Bounds.IntersectsWith(pictureBox6.Bounds))
+            {
+                hearts--;
+                lives.Text = "Lives:" + hearts.ToString();
+                x = r.Next(50, 300);
+                pictureBox6.Location = new Point(x, 0);
+            }
+
+            if (floor.Bounds.IntersectsWith(pictureBox7.Bounds))
+            {
+                hearts--;
+                lives.Text = "Lives:" + hearts.ToString();
+                x = r.Next(50, 300);
+                pictureBox7.Location = new Point(x, 0);
+            }
+
+            if (floor.Bounds.IntersectsWith(pictureBox8.Bounds))
+            {
+                hearts--;
+                lives.Text = "Lives:"+hearts.ToString();
+                x = r.Next(50, 300);
+                pictureBox8.Location = new Point(x, 0);
+            }
+
+        
+            lives.Text = "Lives:" + hearts.ToString();
+        }
+        
+       
+    }
+}
